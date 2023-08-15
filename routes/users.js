9c1dc887adb8375912fc8cbde0978187bcd9159b
Igor Oganesian <igor.oganesian@gmail.com>
2023-08-15 13:31:44 -0400

    working on middleware

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,24 @@
 const Router = require("express").Router;
 const router = new Router();
 
+const { BadRequestError, UnauthorizedError } = require("../expressError");
+const User = require("../models/user");
+
+const jwt = require("jsonwebtoken");
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name}, ...]}
  *
  **/
+
+router.get("/",
+  ensureLoggedIn,
+  async function (req, res, next) {
+  const users = await User.all();
+
+  return res.json({ users });
+})
 
 
 /** GET /:username - get detail of users.
@@ -17,6 +29,13 @@ const router = new Router();
  *
  **/
 
+router.get("/:username",
+  ensureLoggedIn,
+  async function (req, res, next) {
+  const users = await User.get(username);
+
+  return res.json({ users });
+})
 
 /** GET /:username/to - get messages to user
  *
@@ -38,5 +57,13 @@ const router = new Router();
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+function ensureLoggedIn(req, res, next) {
+  const user = res.locals.user;
+  if (user && user.username) {
+    return next();
+  }
+  throw new UnauthorizedError();
+}
 
 module.exports = router;
